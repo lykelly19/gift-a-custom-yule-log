caf3c69ae474192b3ef0dc2f9f8af86a134bdbb2
Kelly <lykelly2014@icloud.com>
2020-12-25 20:41:49 -0500

    yule log page added

--- app.py
+++ app.py
@@ -9,5 +9,15 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/yule_log', methods=['POST'])
+def yule_log():
+    return render_template('yule_log.html')
+
+@app.route('/generate_lyrics', methods=['POST'])
+def generate_lyrics():
+    return render_template('generate_lyrics.html')
+
+
+
 if __name__ == '__main__':
     app.run()
